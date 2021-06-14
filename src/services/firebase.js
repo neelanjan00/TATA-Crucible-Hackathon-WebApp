86bfa0799f52f0c9be7f74776758b61358ba005a
Neelanjan Manna <neelanjanmanna@gmail.com>
2021-06-14 21:03:57 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAUU4B_2JkDwUFw-JbQgfqqxq8xkpMUWmE",
-    authDomain: "dmscanner10.firebaseapp.com",
-    databaseURL: "https://dmscanner10-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "dmscanner10",
-    storageBucket: "dmscanner10.appspot.com",
-    messagingSenderId: "614626576227",
-    appId: "1:614626576227:web:e8f4b69e3ad01fb1737556"
+    // apiKey: "",
+    // authDomain: "",
+    // databaseURL: "",
+    // projectId: "",
+    // storageBucket: "",
+    // messagingSenderId: "",
+    // appId: ""
 }
 
 firebase.initializeApp(firebaseConfig)
